1cf48b23f3f173db0e5a158ce0d6af568399e11f
Bai Biram Njie Fog <baibiramnjie41@gmail.com>
2020-06-30 14:58:01 -0400

    commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const About = () => (
+  <div>
+    <h2>History</h2>
+    <p>In the 15th century, Europeans started to explore the river and coast area – the River Gambia soon became a busy trading zone for gold and slaves. During the colonial period, several European powers contested ownership of the river with Britain eventually prevailing, and The Gambia became a British Protectorate in the early 1820s, surrounded by French-ruled Senegal.</p>
+    <p>The drive for Gambian independence gained momentum after World War 2 and in 1961 the first universal franchise election was held, with Dawda Jawara winning. The Gambia became the last of Britain’s West African colonies to attain full independence, mainly because it was so small and poor that doubts surfaced about its economic viability. A merger with Senegal was suggested but rejected when the issue of how to share power could not be resolved. Eventually, in 1965 The Gambia achieved independence and Jawara became the country’s first Prime Minister before assuming the role of President when the country became a republic in 1970 as a member of the Commonwealth.</p>
+    </div>
+)
---
+++ src/visit.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const visit = () => (
+  <div>
+    <h2>Why You Should Come Visit</h2>
+    When the summer season arrives, a typical holidaymaker will flock to the beaches of the Canary Islands, the south of Spain or the Caribbean. Unfortunately everybody else has the same idea, and this often results in crowded beaches and resorts. In The Gambia, it isn’t unusual to have the beach almost exclusively to yourself. And given that the country enjoys year-round sunshine, there’s no need to wait until the peak months to get your fill of sand, sun and sea.
+   </div>
+)
